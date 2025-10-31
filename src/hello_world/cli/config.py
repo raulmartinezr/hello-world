7154e9475e22200b7944abc3dc2d4fdf5095f725
Raul Martinez <raulmart@cloudsynthops.com>
2025-10-31 20:45:28 +0100

    chore: Changes

--- src/hello_world/cli/config.py
+++ src/hello_world/cli/config.py
@@ -1,8 +1,6 @@
-import json
-from pathlib import Path
-
 from pydantic import BaseModel
-
+from pathlib import Path
+import json
 
 class Settings(BaseModel):
     greeting_prefix: str = "Hello"
@@ -12,3 +10,4 @@ class Settings(BaseModel):
         p = Path(path)
         data = json.loads(p.read_text(encoding="utf-8"))
         return cls(**data)
+
